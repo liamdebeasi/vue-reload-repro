2923838f1d318612da87878bd2fe430a1a411b4a
Liam DeBeasi <liamdebeasi@icloud.com>
2021-09-22 11:06:14 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
-  <ion-app>
-    <ion-router-outlet />
-  </ion-app>
+  Hello World!
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    IonApp,
-    IonRouterOutlet
-  }
+  name: 'App'
 });
 </script>
